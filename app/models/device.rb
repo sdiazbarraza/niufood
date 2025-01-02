bbf948adae2f6b332f3410aabba737e288efd9df
Sebastian diaz <sebastiandesarrolla@gmail.com>
2025-01-02 01:39:17 +0000

    feat: create register after save device

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,10 +1,17 @@
 class Device < ApplicationRecord
   belongs_to :restaurant
   has_many :registers, dependent: :destroy
+
+  after_save :create_register
   
   validates :name, presence: true
   validates :status, presence: true
 
   validates :status, presence: true, inclusion: { in: %w(maintenance active), message: "%{value} no es válido para status" }
+  
 
+  private
+    def create_register
+      Register.create(device: self, status: self.status)
+    end
 end
